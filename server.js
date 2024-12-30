25bb0830b257e953adf7055a3e86839f3c45fe72
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-12-30 20:54:11 +0530

    adding functions ...

---
+++ server.js
@@ -0,0 +1,51 @@
+import { Server } from "socket.io"
+
+
+const io=new Server({
+    cors:{
+        origin:"http://localhost:5153",
+    },
+});
+
+
+
+let onlineUsers= [];
+
+const addUser=(userId,socketId)=>{
+   const userExists=onlineUsers.find((user)=> user.userId === userId);
+   if(!userExists){
+      onlineUsers.push({userId,socketId});
+   };
+};
+
+
+const removeUser=(socketId)=>{
+   onlineUsers= onlineUsers.filter((user)=> user.socketId !== socketId);
+};
+
+
+const getUser=(userId)=>{
+   return onlineUsers.find((user)=> user.userId === userId)
+};
+
+io.on("connection",(socket)=>{
+    
+    socket.on("newUser",(userId)=>{
+       addUser(userId, socket.id);
+    });
+
+    socket.on("sendMessage",({ receiverId, data })=>{
+        const receiver= getUser(receiverId);
+        io.to(receiver.socketId).emit("getMessage",data);
+    });
+
+    socket.on("disconnect",()=>{
+        removeUser(socket.id);
+    });
+    
+});
+
+
+
+
+io.listen("4000");
